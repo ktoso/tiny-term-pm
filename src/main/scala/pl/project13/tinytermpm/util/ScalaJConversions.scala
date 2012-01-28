088d2af39de588ef6902124fb06d4b570eeadc30
Konrad Malawski <konrad.malawski@project13.pl>
2012-01-28 15:37:40 +0100

    null safeness

--- src/main/scala/pl/project13/tinytermpm/util/ScalaJConversions.scala
+++ src/main/scala/pl/project13/tinytermpm/util/ScalaJConversions.scala
@@ -1,9 +1,10 @@
 package pl.project13.tinytermpm.util
 
 import scalaj.collection.Imports._
+import java.util.Collections
 
 trait ScalaJConversions {
 
-  implicit def list2list[E](l: java.util.List[E]): scala.List[E] = l.asScala.toList
-  implicit def list2list[E](l: scala.List[E]): java.util.List[E] = l.asJava
+  implicit def list2list[E](l: java.util.List[E]): scala.List[E] = if(l == null) Nil else l.asScala.toList
+  implicit def list2list[E](l: scala.List[E]): java.util.List[E] = if(l == null) Collections.emptyList() else l.asJava
 }
